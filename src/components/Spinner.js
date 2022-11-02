c90e2aa2175ee95bc71271c85c7e0f6ffe7d4ed6
Felipe Coll <devphilipcoll@gmail.com>
2022-11-02 15:49:15 -0300

    Actualizaciones varias en el proyecto

---
+++ src/components/Spinner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../assets/css/Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
